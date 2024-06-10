9e436fa510013ffdbe365d5fbde73cea906634cd
KurultayIlhan <137696953+KurultayIlhan@users.noreply.github.com>
2024-06-10 10:37:50 +0200

    Add files via upload

---
+++ ClubStatDatabase/dbo/Functions/GetNextMatchesForPlayer.sql
@@ -0,0 +1,24 @@
+﻿create   function [dbo].[GetNextMatchesForPlayer](@playerId uniqueidentifier)
+returns table
+as return
+SELECT        
+ M.MatchId
+,M.MatchDate 
+,M.PlayerLeague 
+,M.PlayerLeagueLevel
+,m.[AwayTeamGoals]
+,m.[HomeTeamGoals]
+,a.[ClubId]		 as AwayClubId
+,a.[ClubName]	 as AwayClubName
+,a.[ClubIconUrl] as AwayClubIconUrl
+,a.[ClubCity]	 as AwayCity
+,h.[ClubId]		 as HomeClubId
+,h.[ClubName]	 as HomeClubName
+,h.[ClubIconUrl] as HomeClubIconUrl
+,h.[ClubCity]	 as HomeCity
+FROM  dbo.[Match] as M 
+INNER JOIN dbo.Clubs AS a ON M.AwayTeamClubId = a.ClubId 
+INNER JOIN dbo.Clubs AS h ON M.HomeTeamClubId = h.ClubId
+INNER JOIN dbo.Players AS p ON p.ClubId IN (M.AwayTeamClubId, M.HomeTeamClubId)
+WHERE p.PlayerId = @playerId
+and M.[MatchDate] > Getdate()
---
+++ ClubStatDatabase/dbo/Functions/PlayersMovements.sql
@@ -0,0 +1,43 @@
+﻿CREATE FUNCTION [dbo].[PlayersMovements](@PlayerId uniqueidentifier, @MatchId int)
+RETURNS TABLE
+AS
+RETURN
+(
+    WITH PlayerLocationWithPrevious AS (
+        SELECT 
+            pl.RecordedUtc,
+            pl.Location,
+            LAG(pl.Location) OVER (PARTITION BY pl.GameId, pl.PlayerId ORDER BY pl.RecordedUtc) AS PreviousLocation,
+            LAG(pl.RecordedUtc) OVER (PARTITION BY pl.GameId, pl.PlayerId ORDER BY pl.RecordedUtc) AS PreviousRecordedUtc
+        FROM 
+            dbo.PlayerLocation pl
+        WHERE 
+            pl.PlayerId = @PlayerId 
+            AND pl.GameId = @MatchId
+    )
+    SELECT 
+        @MatchId as MatchId,
+        @PlayerId as PlayerId ,
+        pl.RecordedUtc,
+        pl.Location,
+        CASE 
+            WHEN pl.PreviousLocation IS NOT NULL AND pl.PreviousRecordedUtc IS NOT NULL THEN 
+                (pl.PreviousLocation.STDistance(pl.Location) / 
+                DATEDIFF(SECOND, pl.PreviousRecordedUtc, pl.RecordedUtc)) * 3.6 
+            ELSE NULL
+        END AS Speed, --To convert meters per second (m/s) to kilometers per hour (km/h), you can use the conversion factor 1m/s=3.6km/h 1m/s=3.6km/h.
+        CASE 
+            WHEN pl.PreviousLocation IS NOT NULL AND pl.PreviousRecordedUtc IS NOT NULL THEN 
+                DEGREES(
+                    ATN2(
+                        SIN(RADIANS(pl.Location.Long - pl.PreviousLocation.Long)) * COS(RADIANS(pl.Location.Lat)),
+                        COS(RADIANS(pl.PreviousLocation.Lat)) * SIN(RADIANS(pl.Location.Lat)) - 
+                        SIN(RADIANS(pl.PreviousLocation.Lat)) * COS(RADIANS(pl.Location.Lat)) * 
+                        COS(RADIANS(pl.Location.Long - pl.PreviousLocation.Long))
+                    )
+                )
+            ELSE NULL
+        END AS Direction
+    FROM 
+        PlayerLocationWithPrevious pl
+);
