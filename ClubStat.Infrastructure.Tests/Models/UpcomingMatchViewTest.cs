10c2cba9fee5e66bf81d2b3a5319eecbffe9be34
KurultayIlhan <137696953+KurultayIlhan@users.noreply.github.com>
2024-06-10 10:21:32 +0200

    Updating compilor errors.
    Updating the test upcoming match view test to remove the compiler errors

--- ClubStat.Infrastructure.Tests/Models/UpcomingMatchViewTest.cs
+++ ClubStat.Infrastructure.Tests/Models/UpcomingMatchViewTest.cs
@@ -1,14 +1,25 @@
 ﻿namespace ClubStat.Infrastructure.Models.Tests
 {
+    using ClubStat.Infrastructure.Builder;
+    using ClubStat.Infrastructure.Factories;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+     
     [TestClass()]
     public class UpcomingMatchViewTest
     {
         [TestMethod()]
-        public void ConstructorTest()
+          public async Task ConstructorTest()
         {
-            var sut = new UpcomingMatchView();
-            Assert.IsNotNull(sut);
-            Assert.AreEqual(Division.None,sut.Division);
-        }
+           var config = new ConfigurationBuilder().AddJsonFile("Appsettings.json").Build();
+            using var service = new ServiceCollection()
+                            .RegisterClubStats(config).BuildServiceProvider();
+            var user = await service.GetRequiredService<ILoginFactory>().Login("Ronaldo", "Ronaldo").ConfigureAwait(false);
+            Assert.IsNotNull(user);
+            var sut = service.GetRequiredService<IMatchFactory>();
+            var match = await sut.GetPlyersNextMatch(user.UserId);
+            Assert.IsNotNull(match);
+            Assert.AreNotEqual(match.AwayTeam.ClubId, match.HomeTeam.ClubId);
+          }
     }
 }
